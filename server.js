4987989824127b030727acf3b4828abbd9efb984
mproyyan <mproyyan@gmail.com>
2024-06-07 07:14:22 +0700

    feat: create destination endpoints

--- server.js
+++ server.js
@@ -1,13 +1,112 @@
-import express from "express"
 import dotenv from "dotenv"
+import express from "express"
+import { PrismaClient } from "@prisma/client"
 
 dotenv.config()
 
-const app = express()
-const port = process.env.APP_PORT
+BigInt.prototype.toJSON = function () {
+    return this.toString()
+}
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World')
+const app = express()
+const port = process.env.APP_PORT || 3000
+const prisma = new PrismaClient()
+
+app.use(express.json())
+
+app.get('/destinations/nearby', async (req, res) => {
+    const { latitude, longitude, radius } = req.query;
+    if (!latitude || !longitude || !radius) {
+        return res.status(400).json({ error: 'Parameters required' });
+    }
+
+    const userLatitude = parseFloat(latitude);
+    const userLongitude = parseFloat(longitude);
+    const userRadius = parseInt(radius)
+
+    try {
+        const nearbyDestinations = await prisma.$queryRaw`
+            SELECT
+                id,
+                name,
+                slug,
+                description,
+                image,
+                latitude,
+                longitude,
+                (6371 * acos(
+                    cos(radians(${userLatitude}))
+                    * cos(radians(latitude))
+                    * cos(radians(longitude) - radians(${userLongitude}))
+                    + sin(radians(${userLatitude}))
+                    * sin(radians(latitude))
+                )) AS distance,
+                 created_at,
+                 updated_at
+            FROM
+                destinations
+            HAVING
+                distance <= ${userRadius}
+            ORDER BY
+                distance;
+        `
+
+        res.json({
+            data: {
+                destinations: nearbyDestinations
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+app.get('/destinations/search', async (req, res) => {
+    const { name } = req.query;
+    if (!name) {
+        return res.status(400).json({ error: 'Name parameter is required' });
+    }
+
+    try {
+        const destinations = await prisma.destination.findMany({
+            where: {
+                name: {
+                    contains: name,
+                    // mode: Prisma.QueryMode.insensitive,
+                },
+            },
+        });
+
+        res.json({
+            data: {
+                destinations: destinations
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+app.get('/destinations/:slug', async (req, res) => {
+    const { slug } = req.params
+
+    try {
+        const destination = await prisma.destination.findUnique({
+            where: { slug: slug }
+        })
+
+        if (!destination) {
+            return res.status(404).json({ error: 'Destination not found' });
+        }
+
+        res.json({
+            data: {
+                destination: destination
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 })
 
 app.listen(port, () => {
